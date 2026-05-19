aa98116d5a64532871ddeb18a35891c6cf833714
miniskies <100922833+miniskies@users.noreply.github.com>
2026-05-19 17:25:27 -0600

    Update skz_data_v2.js

--- SKZ Song Sorting_files/skz_data_v2.js
+++ SKZ Song Sorting_files/skz_data_v2.js
@@ -2,8 +2,8 @@
 // 2009/1/27 Modified by K-Factory@migiwa
 
 // *****************************************************************************
-str_CenterT = 'Tie!';
-str_CenterB = 'Undo last choice';
+str_CenterT = 'Tie';
+str_CenterB = 'Undo';
 
 str_ImgPath = 'http://i.imgur.com/';
 // 0:順番に　1:昔の
